b303302db0d4d1c57925fdfcac0b874e242be832
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2026-04-15 17:10:49 -0300

    adicionada aula 04-13

---
+++ bimestre1/aula_04-13/main.py
@@ -0,0 +1,16 @@
+from ListaCont import Lista
+
+lista = Lista(10)
+lista.InserirOtimizado(1,2)
+lista.Exibir()
+lista.InserirOtimizado(2,3)
+lista.Exibir()
+lista.InserirOtimizado(2,44)
+lista.Exibir()
+lista.InserirOtimizado(2,55)
+lista.Exibir()
+lista.InserirOtimizado(2,66)
+lista.Exibir()
+
+lista.Remover(3)
+lista.Exibir()
